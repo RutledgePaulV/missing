b59551b0a3fa142a9def5fc3e6e9becfb9b9f293
Paul Rutledge <rutledgepaulv@gmail.com>
2020-04-22 23:57:56 -0500

    fork and add some new functions

--- project.clj
+++ project.clj
@@ -1,43 +1,28 @@
-(defproject com.vodori/missing "0.1.29-SNAPSHOT"
+(defproject org.clojars.rutledgepaulv/missing "0.1.29-SNAPSHOT"
 
   :description
   "A utility library for Clojure of functions and macros that are frequently missed and recreated."
 
   :url
-  "https://github.com/vodori/missing"
+  "https://github.com/rutledgepaulv/missing"
 
   :license
   {:name "MIT License" :url "http://opensource.org/licenses/MIT" :year 2020 :key "mit"}
 
   :scm
-  {:name "git" :url "https://github.com/vodori/missing"}
+  {:name "git" :url "https://github.com/rutledgepaulv/missing"}
 
   :pom-addition
   [:developers
    [:developer
-    [:name "Julia Cicale"]
-    [:url "https://github.com/jcicale"]
-    [:email "julia.cicale@vodori.com"]
-    [:timezone "-5"]]
-   [:developer
-    [:name "Griffin Lacek"]
-    [:url "https://github.com/griffinlacek"]
-    [:email "griffin.lacek@vodori.com"]
-    [:timezone "-5"]]
-   [:developer
     [:name "Paul Rutledge"]
     [:url "https://github.com/rutledgepaulv"]
-    [:email "paul.rutledge@vodori.com"]
-    [:timezone "-5"]]
-   [:developer
-    [:name "Travis Stom"]
-    [:url "https://github.com/travisstom"]
-    [:email "travis.stom@vodori.com"]
-    [:timezone "-4"]]]
+    [:email "rutledgepaulv@gmail.com"]
+    [:timezone "-5"]]]
 
   :deploy-repositories
-  {"releases"  {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/" :creds :gpg}
-   "snapshots" {:url "https://oss.sonatype.org/content/repositories/snapshots/" :creds :gpg}}
+  [["releases" :clojars]
+   ["snapshots" :clojars]]
 
   :plugins
   [[lein-cloverage "1.1.2"]]

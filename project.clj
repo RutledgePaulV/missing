3ad08d1afc7b7f454b3aee6a5913820dc81e4b2e
Paul Rutledge <rutledgepaulv@gmail.com>
2020-12-01 22:57:49 -0600

    Version 0.1.37-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.rutledgepaulv/missing "0.1.36"
+(defproject org.clojars.rutledgepaulv/missing "0.1.37-SNAPSHOT"
 
   :description
   "A utility library for Clojure of functions and macros that are frequently missed and recreated."
